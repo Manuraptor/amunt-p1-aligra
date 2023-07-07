b24cbd07f18592b747d2315af0c72877570ca1ec
Manuraptor <majejuma@hotmail.com>
2023-07-07 13:34:27 +0200

    footer

--- src/lib/componentes/Footer.svelte
+++ src/lib/componentes/Footer.svelte
@@ -1,1 +1,36 @@
-<p>esto va</p>
+
+<section>
+    <p> </p>
+
+</section>
+
+<div>
+    <p>Contacto</p><p>Politica de Privacidad</p><p>Acerca de</p><p>Ayuda</p>
+
+</div>
+
+<style>
+
+    section{
+             width: 100%;
+             height: 39px;
+             background-color: #FCAF3B;
+            }
+
+    div {
+        width: 100%;
+        height: 183px;
+        Background-color: black;
+        color: #FCAF3B;
+        display: flex;
+        flex-direction: row;
+        Align-items: center;
+        Justify-content: center;
+        gap:105px;
+        font-size: 25px;
+        font-style: Oblique;
+
+        font-family: 'Ubuntu Mono', monospace;    }
+
+
+</style>
