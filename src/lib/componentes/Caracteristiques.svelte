ea0299272132f32e36741dc13605ed3c5a094fd8
Manuraptor <majejuma@hotmail.com>
2023-07-07 15:01:30 +0200

    caracteristiquesprimerospasos

---
+++ src/lib/componentes/Caracteristiques.svelte
@@ -0,0 +1,28 @@
+<section>
+ <article>
+     <p>  Aligra està concebut per adaptar-se a una gran diversitat de situacions,
+         tant a escala arquitectònica com a nivell de paisatge urbà.</p>
+     <h3>Les principals característiques del sistema Aligra són les següents:</h3>
+ </article>
+    <div class="containermediano">
+    <div>Aqui van 1 cuadrado y 1 parrafo</div><div class="image">FOTO</div>
+        <img class="square" src="src/lib/imagenes/Caracteristiques/Rectangle 29.jpg" alt="decoración cuadrado" />
+    </div>
+    <div>Aqui van 1 cuadrado y 1 parrafo</div>
+    <img class="square"  src="src/lib/imagenes/Caracteristiques/Rectangle 27.jpg" alt="decoración cuadrado" />
+
+    <div class="containermediano">
+    <div class="image">FOTO</div><div>Aqui van 1 cuadrado y 1 parrafo</div>
+        <img class="square" src="src/lib/imagenes/Caracteristiques/Rectangle 19.jpg" alt="decoración cuadrado" />
+        <div>Aqui van 1 cuadrado y 1 parrafo</div>
+        <img class="square" src="src/lib/imagenes/Caracteristiques/Rectangle 18.jpg" alt="decoración cuadrado" />
+    </div>
+
+    <div class="containermediano">
+    <div>Aqui van 1 cuadrado y 1 parrafo</div><div class="image">FOTO</div>
+        <img class="square" src="src/lib/imagenes/Caracteristiques/Rectangle 15.jpg" alt="decoración cuadrado" />
+        <div>Aqui van 1 cuadrado y 1 parrafo</div>
+        <img class="square" src="src/lib/imagenes/Caracteristiques/Rectangle 14.jpg" alt="decoración cuadrado" />
+    </div>
+
+</section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
     import Footer from "/src/lib/componentes/Footer.svelte"
 
+    import Caracteristiques from "/src/lib/componentes/Caracteristiques.svelte"
 
     import Navbar from "$lib/componentes/Navbar.svelte";
 
 </script>
 <Navbar />
 
+<Caracteristiques />
 <slot />
 <Footer />
