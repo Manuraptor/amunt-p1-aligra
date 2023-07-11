540fdeca017251daec1d14570048508ce720f3bf
jutelma <jutelma07051980@gmail.com>
2023-07-11 14:28:01 +0200

    Se ha dado estilos a la seccion kits

--- src/routes/kits/+page.svelte
+++ src/routes/kits/+page.svelte
@@ -1,33 +1,36 @@
 <script>
      import  ImgKit1  from "$lib/imagenes/kits/ImgKit1.jpg";
-     import  ImgKit2  from "$lib/imagenes/kits/ImgKit1.jpg";
-     import  ImgKit3  from "$lib/imagenes/kits/ImgKit1.jpg";
-     import  ImgKit4  from "$lib/imagenes/kits/ImgKit1.jpg";
-     import  ImgKit5  from "$lib/imagenes/kits/ImgKit1.jpg";
+     import  ImgKit2  from "$lib/imagenes/kits/ImgKit2.jpg";
+     import  ImgKit3  from "$lib/imagenes/kits/ImgKit3.jpg";
+     import  ImgKit4  from "$lib/imagenes/kits/ImgKit4.jpg";
+     import  ImgKit5  from "$lib/imagenes/kits/ImgKit5.jpg";
+     import '/src/global.css'
 </script>
 
-<header>
+<section>
+
     <h1>Parts que componen el sistema Aligra</h1>
-    <div>
+    <div class="articulos">
+
         <img src={ImgKit1} alt="kits">
-
-        <p>Kit 1: d’ampliació de volum
-        Modularitat adaptable. Es tracta d'un sistema modular bidimensional, lleuger i en sec, desmuntable i reutilitzable, basat principalment en fusta microlaminada. La modulació proposada permet resoldre fàcilment diverses mides d'ampliació.
-        Economia material. El sistema es diferencia d'altres solucions basades en fusta per la reducció de l’ús de material, en un exercici d'optimització radical dels recursos mitjançant l'aprofitament de les tècniques més avançades de la indústria de la fusta.
-        Prestacions estructurals i acústiques. La combinació d'un entramat lleuger de fusta amb una capa de compressió de formigó armat colꞏlaborant permet millorar alhora les prestacions estructurals i acústiques del mòdul de forjat.
+        <p>· Kit 1: d’ampliació de volum<br>
+              · Modularitat adaptable. Es tracta d'un sistema modular bidimensional, lleuger i en sec,<br>
+                desmuntable i reutilitzable, basat principalment en fusta microlaminada. La modulació <br>
+                proposada permet resoldre fàcilment diverses mides d'ampliació.<br>
+              · Economia material. El sistema es diferencia d'altres solucions basades en fusta per la<br>
+                reducció de l’ús de material, en un exercici d'optimització radical dels recursos mitjançant<br>
+                l'aprofitament de les tècniques més avançades de la indústria de la fusta.<br>
         </p>
-    </div>
 
-    <div>
-        <img src={ImgKit2} alt="kits">
+
         <p>Kit 2: d’envolupant bioclimàtica
             Sistema modular amb diferents combinacions possibles, adaptable, segons s'agreguin o substitueixin materials, per a obra nova, rehabilitació, espais habitables i espais bioclimàtics.
             Es diferencia de les alternatives existents en el mercat (SATEs de construcció in situ, humida, amb materials d'alt impacte ambiental i no recuperables) per la seva concepció en sec, reversible i amb materials recuperables.
             Sistema industrialitzat off-site, fabricat en taller, dissenyat en tecnologia BIM i fabricat mitjançant CAD-CAM i muntat en sec amb unions reversibles.
         </p>
-    </div>
+        <img src={ImgKit2} alt="kits">
 
-    <div>
+
         <img src={ImgKit3} alt="kits">
         <p>Kit aljub
             Sistema bicapa compost, que integra una safata aljub estructural que pot suportar dos tipus de capa d'acabat superior: una vegetal i una altra transitable, sense perjudici de que en el futur es puguin desenvolupar d’altres tipus.
@@ -36,18 +39,16 @@
             Excés d’aigua: l'aljub porta un sobreeixidor per a la descàrrega d'aigua cap als baixants pluvials.
             Adaptabilitat: és monitoritzable i ajustable en quantitat i freqüència de reg per l'usuaria través de l'app del kit smart building.
         </p>
-    </div>
 
-    <div>
-        <img src={ImgKit4} alt="kits">
         <p>Kit fotovoltaic
             Sistema modular que permetrà la creació d'una comunitat energètica que podrà arribar a subministrar energia per a tot l'edifici.
             Solució completament integrada que redueix els problemes associats a la instalꞏlació d'aquesta mena de sistemes en edificis d'habitatges (espai, facilitat d'instalꞏlació, constitució de la comunitat…).
             Pèrgola fotovoltaica que permetrà habilitar nous espais ombrejats en la coberta, sense perdre superfície disponible.
 
         </p>
-    </div>
-    <div>
+        <img src={ImgKit4} alt="kits">
+
+
         <img src={ImgKit5} alt="kits">
         <p>Kit Smart building
             Monitoratge i control. El kit Smart building assumeix el monitoratge constant dels indicadors aportats per múltiples sensors sobre els rendiments ambientals, hídrics i energètics de la pell bioclimàtica, de les plaques fotovoltaiques i de l'aljub. Quan és necessari, també executa comandaments automàtics de control de les instalꞏlacions per tal de millorar aquests rendiments. El kit inclou tota la infraestructura necessària per assumir aquestes funcions.
@@ -56,5 +57,31 @@
         </p>
     </div>
 
-</header>
+</section>
+
+<style>
+    h1{
+        font-size: 24px;
+    }
+
+    .articulos {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr) ;
+        margin: 50px 50px;
+
+
+    }
+    img {
+        width: 700px;
+        height: 350px;
+    }
+    p{
+        padding: 20px 20px;
+        width: 700px;
+
+    }
+
+
+
+</style>
 
