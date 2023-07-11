ed56823c32e80768084d80a0fb88e1c5e6e4a049
Manuraptor <majejuma@hotmail.com>
2023-07-11 14:06:00 +0200

    coracteristiqueswork4

--- src/routes/caracteristiques/+page.svelte
+++ src/routes/caracteristiques/+page.svelte
@@ -24,12 +24,21 @@
             <div class="contenedormediano3">
                 <img class="square" src={Rectangle29}
                      alt="decoración cuadrado"/>
-                <div>Aqui van 1 cuadrado y 1 parrafo</div>
+                <div class="contenedortexto">-Economia de mitjans.
+
+                    El sistema multicapa, concebut de manera que cada estrat
+                    compleix alhora diverses funcions, ofereix el màxim rendiment
+                    dels materials.</div>
             </div>
             <div class="contenedormediano3">
                 <img class="square" src={Rectangle27}
                      alt="decoración cuadrado"/>
-                <div>Aqui van 1 cuadrado y 1 parrafo</div>
+                <div class="contenedortexto">-Modularitat.
+
+                    Dimensionalment, tant la part física de Aligra com els seus sistemes
+                    de serveis es basen rigorosament en modulatges compatibles amb els
+                    estàndars industrials i tecnològics més universals.
+                </div>
             </div>
         </div>
         <img src={Foto2} alt="foto"/>
@@ -41,12 +50,20 @@
         <div class="contenedormediano2">
             <div class="contenedormediano3">
 
-                <div>Aqui van 1 cuadrado y 1 parrafo</div>
+                <div class="contenedortexto">-Facilitat de muntatge.
+
+                    La combinació de lleugeresa i modularitat, juntament
+                    amb l’ús d’unions reversibles i accessibles, facilita          notablement el seu muntatge i desmuntatge.
+                </div>
                 <img class="square" src={Rectangle18}
                      alt="decoración cuadrado"/>
             </div>
             <div class="contenedormediano3">
-                <div>Aqui van 1 cuadrado y 1 parrafo</div>
+                <div class="contenedortexto">-Lleugeresa.
+
+                    La selecció acurada dels materials i l’aprofitament òptim de
+                    les seves prestacions permet millorar els processos de muntatge i desmuntatge
+                    en matèria de seguretat, inclusivitat i estalvi energètic.</div>
                 <img class="square" src={Rectangle19}
                      alt="decoración cuadrado"/>
             </div>
@@ -57,14 +74,22 @@
             <div class="contenedormediano3">
                 <img class="square" src={Rectangle14}
                      alt="decoración cuadrado"/>
-                <div>Aqui van 1 cuadrado y 1 parrafo</div>
+                <div class="contenedortexto">-Materials ecològics.
+
+                    El sistema utilitzat materials negatius en CO2, no emissors de COVs,
+                    higroscòpics,i materials que no retenen ni propaguen  radiació electromagnètica.
+                </div>
 
 
             </div>
             <div class="contenedormediano3">
                 <img class="square" src={Rectangle15}
                      alt="decoración cuadrado"/>
-                <div>Aqui van 1 cuadrado y 1 parrafo</div>
+                <div class="contenedortexto">-Flexibilitat.
+
+                    En oferir una àmplia gamma de materials i gruixos, el sistema facilita
+                    l’adaptació a eventuals modificacions amb la possible participació
+                    dels futurs usuaris.</div>
             </div>
         </div>
         <img src={Foto3}
@@ -75,6 +100,20 @@
 
 <style>
 
+    section {
+
+        width:1200px;
+        height: 1400px;
+    }
+
+        .contenedortexto{
+
+            height: 100px;
+            width: 565px;
+            display: flex;
+            align-items: center;
+
+        }
     .contenedormediano {
         display: flex;
         flex-direction: row;
@@ -88,6 +127,10 @@
     .contenedormediano3 {
         display: flex;
         flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        height: 100px;
+        width:725px;
     }
 
 </style>
