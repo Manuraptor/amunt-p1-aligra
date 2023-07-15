9f8936b1646e78576537823f7d2dcca81de16744
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-07-15 13:38:36 +0200

    creando componente para los articulos de la ruta Que es Aligra

---
+++ src/lib/componentes/ArticleQueEsAligra.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<article>
+    <p>
+        <slot></slot>
+    </p>
+</article>
+
+<style>
+    article {
+        background-color: #f5f5f5;
+        border-radius: 10px;
+        padding: 20px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        text-align: justify;
+    }
+
+    p {
+        font-size: 16px;
+        line-height: 1.5;
+    }
+
+
+</style>
