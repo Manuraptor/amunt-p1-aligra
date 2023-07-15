5472b3934dfa5cbcf0938879ad481d21da3ec77a
Manuraptor <majejuma@hotmail.com>
2023-07-15 10:16:10 +0200

    cambiosmenoreshaciadiseño7

--- src/lib/componentes/Componente.svelte
+++ src/lib/componentes/Componente.svelte
@@ -1,33 +1,22 @@
 <script>
+export let imagen
+export let texto
 
-    import straddle3 from "$lib/imagenes/Colaboradors/image 1.png";
+
 </script>
 
+<section>
+    <img src={imagen} alt="imagen">
+    <p>{texto}</p>
 
+</section>
 
-    <div class="textoylogo">
-        <img src={straddle3}
-             alt="decoración cuadrado"/>
-        <div class="contenedortexto">
-
-            Straddle3:  Colꞏlectiu de Barcelona que treballa en arquitectura<br>
-            i ciutat des d'una perspectiva crítica i inclusiva.<br>
-            <br>
-            Ha desenvolupat projectes d'habitatge social, processos participatius,<br>
-            intervencions en espais públics i recerca en la  construcció a  partir de<br>
-            materials reutilitzats i reciclats.
-        </div>
-    </div>
 
 
 <style>
 
-    main {
-        width: 100%;
-        padding-top: 180px;
-    }
 
-    .textoylogo{
+    section{
 
         display: flex;
         flex-direction: row;
@@ -38,14 +27,5 @@
         margin-left: 275px;
     }
 
-    .contenedortexto{
-
-
-        display: flex;
-        align-items: center;
-        height: 100px;
-        width: 730px;
-
-    }
 
 </style>
--- src/routes/colaboradors/+page.svelte
+++ src/routes/colaboradors/+page.svelte
@@ -2,16 +2,25 @@
     import '/src/global.css'
 
     import Componente from "$lib/componentes/Componente.svelte";
+    import straddle3 from "$lib/imagenes/Colaboradors/image 1.png";
 
 
 </script>
 
-<main>
 
-    <Componente/>
+    <Componente  imagen="{straddle3}" texto=" Straddle3:  Colꞏlectiu de Barcelona que treballa en arquitectura
+                                i ciutat des d'una perspectiva crítica i inclusiva.
+
+                                Ha desenvolupat projectes d'habitatge social, processos participatius,
+                                intervencions en espais públics i recerca en la  construcció a  partir de
+                                materials reutilitzats i reciclats."/>
 
 
-</main>
+
+
+
+
+
 
 
 <style>
@@ -21,25 +30,25 @@
         padding-top: 180px;
     }
 
- .textoylogo{
+    .textoylogo{
 
-      display: flex;
-     flex-direction: row;
-     justify-content: space-around;
-     height: 100px;
-     width: 900px;
-     gap:85px;
-     margin-left: 275px;
- }
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        height: 100px;
+        width: 900px;
+        gap:85px;
+        margin-left: 275px;
+    }
 
- .contenedortexto{
+    .contenedortexto{
 
 
-     display: flex;
-     align-items: center;
-     height: 100px;
-     width: 730px;
+        display: flex;
+        align-items: center;
+        height: 100px;
+        width: 730px;
 
- }
+    }
 
 </style>
