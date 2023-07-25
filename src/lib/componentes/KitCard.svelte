8be4529c88219f2d1396fa31f14282e37a4800dd
jutelma <jutelma07051980@gmail.com>
2023-07-26 00:47:44 +0200

    solucionando conflicto

--- src/lib/componentes/KitCard.svelte
+++ src/lib/componentes/KitCard.svelte
@@ -23,14 +23,14 @@
         flex-direction: column;
         align-items: center;
         justify-content: space-around;
-<<<<<<< HEAD
+
 
     }
 
     h1 {
         font-family: Impact, SansSerif;
         margin-bottom: 1rem;
-=======
+
         box-shadow: 5px 5px 5px #cccccc;
 
     }
@@ -45,10 +45,6 @@
 
     }
 
-    h3 {
-        margin-bottom: 0.5rem;
->>>>>>> bff506c379325ba0d324fa22268d3236820ce74a
-    }
 
 
     a {
@@ -61,12 +57,10 @@
         width: 100%;
         object-fit: cover;
         border-radius: 8px;
-<<<<<<< HEAD
         margin-top: 100px;
         margin-bottom: 10px;
-=======
-        margin-top: 90px;
->>>>>>> bff506c379325ba0d324fa22268d3236820ce74a
+
+
     }
 
 
