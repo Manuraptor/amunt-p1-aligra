1eb64ed051b5e1ca970cd108f3517774aea97218
jutelma <jutelma07051980@gmail.com>
2023-07-25 13:17:55 +0200

    estilos y añadiendo imagenes de kits

--- src/lib/componentes/KitCard.svelte
+++ src/lib/componentes/KitCard.svelte
@@ -2,15 +2,18 @@
 <script>
     export let title;
     export let link;
+    export let image
 </script>
 
 <div class="KitCard">
     <h3>{title}</h3>
+    <img src={image} alt="kit 1"/>
     <a href={link}>Ver más</a>
 </div>
 
 <style>
     .KitCard {
+        background-color: #f5f5f5;
         border: 1px solid #ccc;
         padding: 1rem;
         margin: 1rem;
@@ -18,14 +21,34 @@
         display: flex;
         flex-direction: column;
         align-items: center;
+        justify-content: space-around;
+        box-shadow: 5px 5px 5px  #cccccc;
+
     }
 
+    .KitCard:hover {
+        background-color: #FCB900;
+    }
+    .KitCard:hover a {
+
+               color: white;
+
+    }
     h3 {
         margin-bottom: 0.5rem;
     }
 
+
     a {
-        color: blue;
+        color: black;
         text-decoration: none;
+
     }
+    img {
+        width: 100%;
+        object-fit: cover;
+        border-radius: 8px;
+        margin-top: auto; /* Mueve la imagen hacia abajo */
+    }
+
 </style>
--- src/routes/kits/+page.svelte
+++ src/routes/kits/+page.svelte
@@ -1,29 +1,38 @@
 
 <script>
     import KitCard from "$lib/componentes/KitCard.svelte";
-
+    import Imagen1 from "$lib/imagenes/kits/ImgKit6.jpg";
+    import Imagen2 from "$lib/imagenes/kits/ImgKit7.jpg";
+    import Imagen3 from "$lib/imagenes/kits/ImgKit8.jpg";
+    import Imagen4 from "$lib/imagenes/kits/ImgKit9.jpg";
+    import Imagen5 from "$lib/imagenes/kits/ImgKit10.jpg"
 
 
 
     const kits = [
         {
             title: 'KIT 1',
+            image: Imagen1,
             link: '/kitPage1',
         },
         {
             title: 'KIT 2 ',
+            image: Imagen2 ,
             link: '/kitPage2',
         },
         {
             title: 'KIT 3 ',
+            image: Imagen3,
             link: '/kitPage3',
         },
         {
-            title: 'KIT 4 ',
+            title: 'KIT 4',
+            image: Imagen4 ,
             link: '/kitPage4',
         },
         {
             title: 'KIT 5',
+            image: Imagen5 ,
             link: '/kitPage5',
         },
     ];
@@ -36,17 +45,18 @@
 <main>
      {#each kits as kit}
 
-         <KitCard title={kit.title} link={kit.link} />
+         <KitCard title={kit.title} image="{kit.image}" link={kit.link} />
      {/each}
 
 </main>
 
 <style>
+
     main {
         display: grid;
         grid-template-columns: repeat(5, 1fr);
+        grid-template-rows: repeat(5, 100%);
         gap: 20px;
-        padding: 1em;
-        grid-template-rows: repeat(5, 200%);
+
     }
 </style>
